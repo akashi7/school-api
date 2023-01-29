0ba45bff224af30b815a6f214ac3c5ed12a39870
Brian Gitego <gitegob7@gmail.com>
2023-01-29 17:38:53 +0200

    change on promotion

--- src/student/dto/update-student.dto.ts
+++ src/student/dto/update-student.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from "@nestjs/swagger";
+import { Exclude } from "class-transformer";
 import { CreateStudentDto } from "./create-student.dto";
 
-export class UpdateStudentDto extends PartialType(CreateStudentDto) {}
+export class UpdateStudentDto extends PartialType(CreateStudentDto) {
+  @Exclude()
+  academicYearId: string;
+  @Exclude()
+  streamId: string;
+}
