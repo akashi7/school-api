08ccfffcf60a9b8fd67c17d8a756e63b18082d27
Brian Gitego <gitegob@gmail.com>
2023-04-09 17:01:22 +0200

    fix:update student academic year and streeam

--- src/student/dto/update-student.dto.ts
+++ src/student/dto/update-student.dto.ts
@@ -1,10 +1,4 @@
 import { PartialType } from "@nestjs/swagger";
-import { Exclude } from "class-transformer";
 import { CreateStudentDto } from "./create-student.dto";
 
-export class UpdateStudentDto extends PartialType(CreateStudentDto) {
-  @Exclude()
-  academicYearId: string;
-  @Exclude()
-  streamId: string;
-}
+export class UpdateStudentDto extends PartialType(CreateStudentDto) {}
